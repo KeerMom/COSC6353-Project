5006b5e55ad6a347a8fbcf6b61ffff489f68a988
Svapnarajsinh-Bhadola <55594171+Svapnarajsinh-Bhadola@users.noreply.github.com>
2021-08-04 13:50:01 -0500

    Styles Aboutus css

--- website/auth.py
+++ website/auth.py
@@ -62,8 +62,7 @@ def sign_up():
             return redirect(url_for('views.create_profile'))
 
     return render_template("sign_up.html", user=current_user)
-@auth.route('/Aboutus', methods=['GET', 'POST'])
-def Aboutus():
-    return redirect(url_for('auth.Aboutus'))
+
+
 
 
